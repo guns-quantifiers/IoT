e1c921ab2dcb86b91bb7decf09c3708269c23be0
Mateusz.Galasinski <mateusz.galasinski@protonmail.com>
2019-12-26 19:08:43 +0100

    Fixed version controller

--- src/BlackjackAPI/Controllers/VersionController.cs
+++ src/BlackjackAPI/Controllers/VersionController.cs
@@ -4,10 +4,14 @@
 
 namespace BlackjackAPI.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class VersionController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        [HttpGet]
+        [Route("")]
+        public ActionResult<string> Get()
         {
             return Ok("1.0.1");
         }
